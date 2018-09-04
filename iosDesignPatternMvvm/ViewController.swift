eeb24e116a2d349c7f490787a06b1cf94bfe7843
ysk1031 <yusukeaono.1031@gmail.com>
2018-09-04 18:58:46 +0900

    Initial Commit

---
+++ iosDesignPatternMvvm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosDesignPatternMvvm
+//
+//  Created by Yusuke Aono on 2018/09/04.
+//  Copyright © 2018 Yusuke Aono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
